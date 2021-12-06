1d61e70220e8eadac825930ec86ab0c74067738c
Nikolas Giannakopoulos <nickgiann@outlook.com>
2021-12-06 22:33:03 +0200

    Final

--- src/Genetic.java
+++ src/Genetic.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
@@ -33,46 +34,21 @@ public class Genetic {
 
     public int [][] reproduce(Chromosome x, Chromosome y){
         Random r = new Random();
-        int intersectionPoint = r.nextInt(boardSize -1 ) + 1;
-        //System.out.println("Intersection point: " + intersectionPoint);
         int [][] childGenes = new int[2][boardSize];
 
-        for(int i=0; i<intersectionPoint; i++)
-        {
-            childGenes[0][i] = y.getGenes()[i];
-            childGenes[1][i] = x.getGenes()[i];
-        }
-//        System.out.println("Now in after intersection point:");
-//        System.out.println(childGenes.length);
-        for(int i=intersectionPoint; i<boardSize; i++)
-        {
-//            System.out.println("x" + i + ": "+ x.getGenes()[i]);
-//            System.out.println("y" + i + ": "+ y.getGenes()[i]);
-            childGenes[0][i] = x.getGenes()[i];
-            childGenes[1][i] = y.getGenes()[i];
+        double probability;
+        for (int i = 0; i < boardSize; i++){
+             probability = Math.random();
+            if (probability < 0.5) {
+                childGenes[0][i] = x.getGenes()[i];
+                childGenes[1][i] = y.getGenes()[i];
+            } else {
+                childGenes[0][i] = y.getGenes()[i];
+                childGenes[1][i] = x.getGenes()[i];
+            }
         }
         return childGenes;
     }
-
-/*    public Chromosome reproduce(Chromosome x, Chromosome y)
-    {
-        Random r = new Random();
-        //Randomly choose the intersection point
-        int intersectionPoint = r.nextInt(7) + 1;
-        int [] childGenes = new int[boardSize];
-        //The child has the left side of the x chromosome up to the intersection point...
-        for(int i=0; i<intersectionPoint; i++)
-        {
-            childGenes[i] = x.getGenes()[i];
-        }
-        //...and the right side of the y chromosome after the intersection point
-        for(int i=intersectionPoint; i<childGenes.length; i++)
-        {
-            childGenes[i] = y.getGenes()[i];
-        }
-        //Dimiourgoume mono ena paidi. Idaniko einai na epitrefoume kai ta dyo
-        return new Chromosome(childGenes);
-    }*/
 
     public Chromosome geneticAlgorithm(int populationSize, double mutationProbability, int minFitness, int maxSteps){
 
@@ -81,7 +57,6 @@ public class Genetic {
         Random r = new Random();
         for(int step=0; step < maxSteps; step++)
         {
-
             ArrayList<Chromosome> newPopulation = new ArrayList<Chromosome>();
             for(int i=0; i < populationSize/2; i++)
             {
@@ -98,16 +73,6 @@ public class Genetic {
                 int [][] children = this.reproduce(x, y);
                 Chromosome childA = new Chromosome(children[0]);
                 Chromosome childB = new Chromosome(children[1]);
-
-//                System.out.print("\nParent 1: ");
-//                x.print();
-//                System.out.print("Parent 2: ");
-//                y.print();
-//                System.out.print("Child A: ");
-//                childA.print();
-//                System.out.print("Child B: ");
-//                childB.print();
-//                System.out.println("\n");
 
                 //One or both children might mutate
                 if(r.nextDouble() < mutationProbability)
@@ -142,54 +107,4 @@ public class Genetic {
         System.out.println("Population size: " + population.size());
         return this.population.get(0);
     }
-
-/*    public Chromosome geneticAlgorithm(int populationSize, double mutationProbability, int minimumFitness, int maximumSteps)
-    {
-        //We initialize the population
-        initializePopulation(populationSize); //The Arraylist containing the chromosomes and the fitnessbounds arraylist are created
-        Random r = new Random();
-        for(int step=0; step < maximumSteps; step++)
-        {
-            //Initialize the new generated population
-            ArrayList<Chromosome> newPopulation = new ArrayList<Chromosome>();
-            for(int i=0; i < populationSize; i++)
-            {
-                //We choose two chromosomes from the population
-                //Due to how fitnessBounds ArrayList is generated, the propability of
-                //selecting a specific chromosome depends on its fitness score
-                int xIndex = this.fitnessBounds.get(r.nextInt(this.fitnessBounds.size()));
-                Chromosome x = this.population.get(xIndex);
-                int yIndex = this.fitnessBounds.get(r.nextInt(this.fitnessBounds.size()));
-                while(yIndex == xIndex)
-                {
-                    yIndex = this.fitnessBounds.get(r.nextInt(this.fitnessBounds.size()));
-                }
-                Chromosome y = this.population.get(yIndex);
-                //We generate the "child" of the two chromosomes
-                Chromosome child = this.reproduce(x, y);
-                //We might then mutate the child
-                if(r.nextDouble() < mutationProbability)
-                {
-                    child.mutate();
-                }
-                //...and finally add it to the new population
-                newPopulation.add(child);
-            }
-            this.population = new ArrayList<Chromosome>(newPopulation);
-
-            //We sort the population so the one with the greater fitness is first
-            Collections.sort(this.population, Collections.reverseOrder());
-            //If the chromosome with the best fitness is acceptable we return it
-            if(this.population.get(0).getFitness() >= minimumFitness)
-            {
-                System.out.println("Finished after " + step + " steps...");
-                return this.population.get(0);
-            }
-            //We update the fitnessBounds arrayList
-            this.updateFitnessBounds();
-        }
-
-        System.out.println("Finished after " + maximumSteps + " steps...");
-        return this.population.get(0);
-    }*/
 }
--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
-        /*System.out.println("Please enter the size of the board: ");
+
         Scanner sc = new Scanner(System.in);
-        int boardSize = sc.nextInt();*/
-        Board board = new Board(8);
-        int boardsize = board.getBoardSize();
-        System.out.println("Boardsize in main: " + boardsize);
+        System.out.println("Please the number of Queens: ");
+        int boardsize = sc.nextInt();
+        Board board = new Board(boardsize);
 
         Genetic g = new Genetic();
         int minFitness = board.getMinFitness();
         System.out.println("Min fitness: " + minFitness);
-        Chromosome x = g.geneticAlgorithm(1000, 0.08, minFitness, 1000);
+        long start = System.currentTimeMillis();
+        Chromosome x = g.geneticAlgorithm(1000, 0.12, minFitness, 1000000000);
+        long end = System.currentTimeMillis();
         x.print();
         System.out.println(x.getFitness());
+        System.out.println("Search time: " + (double)(end - start) / 1000 + " sec.");
     }
 }
